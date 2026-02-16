6e84adea546d1ad8b3cb3ab1b440f723d0ce7393
Fedasa Eyob <165522312+FedhiJob@users.noreply.github.com>
2026-02-16 22:28:47 +0300

    Add API response utility

---
+++ backend/src/utils/api-response.ts
@@ -0,0 +1,14 @@
+import type { Response } from "express";
+
+export const sendSuccess = <T>(
+  res: Response,
+  data: T,
+  message = "OK",
+  statusCode = 200,
+) => {
+  return res.status(statusCode).json({
+    success: true,
+    data,
+    message,
+  });
+};
