b52b1074f82366dccac923bfb96ea597dd216bc8
Fedasa Eyob <165522312+FedhiJob@users.noreply.github.com>
2026-02-16 22:22:42 +0300

    Add error handler middleware

---
+++ backend/src/middleware/error-handler.ts
@@ -0,0 +1,45 @@
+import type { NextFunction, Request, Response } from "express";
+import { Prisma } from "../generated/prisma/client";
+import { ZodError } from "zod";
+import { ApiError } from "../utils/api-error";
+
+export const errorHandler = (
+  error: unknown,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  if (error instanceof ApiError) {
+    return res.status(error.statusCode).json({
+      success: false,
+      data: null,
+      message: error.message,
+      details: error.details,
+    });
+  }
+
+  if (error instanceof ZodError) {
+    return res.status(400).json({
+      success: false,
+      data: null,
+      message: "Validation failed",
+      details: error.flatten().fieldErrors,
+    });
+  }
+
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    if (error.code === "P2002") {
+      return res.status(409).json({
+        success: false,
+        data: null,
+        message: "Unique constraint violation",
+      });
+    }
+  }
+
+  return res.status(500).json({
+    success: false,
+    data: null,
+    message: "Internal server error",
+  });
+};
