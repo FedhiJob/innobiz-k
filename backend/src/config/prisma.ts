6a1017853e4dcd55b2e1adc91f33ba96e07fee59
Fedasa Eyob <165522312+FedhiJob@users.noreply.github.com>
2026-02-16 22:49:40 +0300

    Update Prisma configuration

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,13 +1,28 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 import { PrismaClient } from "../generated/prisma/client";
+import { env } from "./env";
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
+  pgPool?: Pool;
 };
+
+const pgPool =
+  globalForPrisma.pgPool ??
+  new Pool({
+    connectionString: env.DATABASE_URL,
+  });
+
+const adapter = new PrismaPg(pgPool);
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient();
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
+  globalForPrisma.pgPool = pgPool;
 }
