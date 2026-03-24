143a95a949b7932b860c6a39d643aba5d3cdfe83
Fedasa Eyob <165522312+FedhiJob@users.noreply.github.com>
2026-03-24 20:24:51 +0300

    Allow local image host

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,6 +4,34 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "4000",
+        pathname: "/api/updates/media/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "4000",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "4000",
+        pathname: "/api/updates/media/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "4000",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
